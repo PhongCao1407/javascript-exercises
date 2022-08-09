3a505e2a65c975a4c6a464bcbc00a6e41ef79c0a
Phong Cao <phongcao0714@gmail.com>
2022-08-08 22:42:29 -0700

    Finish up to 05

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
 const removeFromArray = function() {
+	args = Array.from(arguments);
+	arr = args[0]
 
+	for (let i = 1; i < args.length; i++){
+		if (arr.includes(args[i])){
+			arr.splice(arr.indexOf(args[i]), 1);
+		}
+	}
+	return arr;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+	if (typeof start !== 'number' || typeof end !== 'number' || start < 0 || end < 0){
+		return "ERROR";
+	}
 
+	let ans = 0;
+	low = Math.min(start, end);
+	high = Math.max(start, end);
+	for (let i = low; i <= high; i++){
+		ans += i;
+	}
+	return ans;
 };
 
 // Do not edit below this line
