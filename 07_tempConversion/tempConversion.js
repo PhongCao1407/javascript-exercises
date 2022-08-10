19fc0fdd47c1b6f094bc732824d3a21531d16d17
Phong Cao <phongcao0714@gmail.com>
2022-08-09 22:34:32 -0700

    Finish up to 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
-
+const ftoc = function(degree) {
+	ans = ((degree-32) * (5/9));
+	if (Number.isInteger(ans)){
+		return ans;
+	}
+	return Number(ans.toFixed(1))
 };
 
-const ctof = function() {
-
+const ctof = function(degree) {
+	ans = (degree*(9/5)+32);
+	if (Number.isInteger(ans)){
+		return ans;
+	}
+	return Number(ans.toFixed(1))
 };
 
 // Do not edit below this line
