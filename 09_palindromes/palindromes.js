07d5bc8ff298aa075f8a48c2289f22268270a36a
Phong Cao <phongcao0714@gmail.com>
2022-08-26 11:39:35 -0700

    Finish 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    start = 0;
+    end = string.length - 1;
+    while (start !== end){
+        if (!(/[a-zA-Z]/).test(string[start])){
+            start += 1;
+            continue;
+        }
+        if (!(/[a-zA-Z]/).test(string[end])){
+            end -= 1;
+            continue;
+        }
+        
+        if (string[start].toLowerCase() != string[end].toLowerCase()){
+            return false;
+        }
+
+        start += 1
+        end -= 1
+    }
+
+    return true;
 
 };
 
