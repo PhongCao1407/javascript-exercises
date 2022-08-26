04273c749853e4b5bbcaf2647679dcfb86d52e42
Phong Cao <phongcao0714@gmail.com>
2022-08-26 11:29:59 -0700

    Finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,45 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  sm = 0;
+	for (let i = 0; i < arr.length; i++){
+    sm += arr[i];
+  }
+  return sm;
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  ans = 1;
+	for (let i = 0; i < arr.length; i++){
+    ans *= arr[i];
+  }
+  return ans;
 };
 
-const power = function() {
-	
+const power = function(num, pw) {
+	ans = 1
+  for (let i = 0; i < pw; i++){
+    ans *= num;
+  }
+  return ans;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0){
+    return 1
+  }
+  ans = num
+  for (let i = num - 1; i > 0; i--){
+    ans *= i;
+  }
+  return ans;
 };
 
 // Do not edit below this line
