9dba83ba6fcde8ad304aa86174c2e8d07ae67c46
Phong Cao <phongcao0714@gmail.com>
2022-08-26 12:57:39 -0700

    Finish 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(obj) {
+    let nme;
+    let age = 0;
 
+    for (prop in obj){
+        if (! ("yearOfDeath" in obj[prop])){
+            if (2022 - obj[prop].yearOfBirth > age){
+                nme = obj[prop].name;
+                age = 2022 - obj[prop].yearOfBirth;
+            }
+        }
+        if (obj[prop].yearOfDeath - obj[prop].yearOfBirth > age){
+            nme = obj[prop].name;
+            age = obj[prop].yearOfDeath - obj[prop].yearOfBirth;
+        }
+    }
+
+    ans = {
+        name: nme
+    };
+    return ans;
 };
 
 // Do not edit below this line
